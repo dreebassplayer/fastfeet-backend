11690db8e71361fd8f420481c1fe966fe4540ba4
André Machado <andre_contra.baixo@hotmail.com>
2020-12-05 13:22:37 -0400

    Project created

---
+++ src/main/java/com/gostack/fastfeetbackend/FastfeetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gostack.fastfeetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastfeetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastfeetBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gostack/fastfeetbackend/FastfeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gostack.fastfeetbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastfeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
