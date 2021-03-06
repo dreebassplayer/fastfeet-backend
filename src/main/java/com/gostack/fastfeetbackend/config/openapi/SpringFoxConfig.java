42eef399bfe3f3aa3d9cea00be041cebc6f2f359
André Machado <andre_contra.baixo@hotmail.com>
2021-03-06 12:31:27 -0400

    [Added] API info

--- src/main/java/com/gostack/fastfeetbackend/config/openapi/SpringFoxConfig.java
+++ src/main/java/com/gostack/fastfeetbackend/config/openapi/SpringFoxConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +21,14 @@ public class SpringFoxConfig  implements WebMvcConfigurer {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.gostack.fastfeetbackend.domain.controller"))
+                .build().apiInfo(apiInfo());
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("FastFeet API")
+                .description("API do brabo, manso e enjoado João Menna")
+                .version("1.0")
                 .build();
     }
 
